f8bb9d7173a3347d0d3f619a5fbe7dbfb457d65e
bahge <leandrobage@gmail.com>
2018-12-19 22:16:17 -0300

    Ex02 - Fixed Bug

--- Aulas/181219/ex02.c
+++ Aulas/181219/ex02.c
@@ -7,21 +7,35 @@
 
 #include <stdio.h>
 
-int potencia(int a, int b) {
-    int resultado, i;
-    resultado = 1;
-    for(i = 1; i < b + 1; i++) {
-        resultado = resultado * a;
+float potencia(int a, int b) {
+    
+    int i;
+    float resultado = 1;
+
+    if ( b < 0 ){
+        for (i=b; i<0;i++){
+            resultado = resultado / a;
+        }
+        if ( b % 2 != 0) {
+            resultado = resultado * -1; 
+        }
+    } 
+    else if (b > 0) {
+        for (i=0; i<b; i++){
+            resultado = resultado * a;
+        }
     }
+    
     return resultado;    
 }
 
 void main() {
-    int base, expoente, resultado;
+    int base, expoente;
+    float resultado;
     printf("Digite a base: \n");
     scanf("%i", &base);
     printf("Digite o expoente: \n");
     scanf("%i", &expoente);
     resultado = potencia(base, expoente);
-    printf("Resultado: %i \n", resultado);
+    printf("Resultado: %.4f \n", resultado);
 }
